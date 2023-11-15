82547fe0522d504daafe011f21f96f5658c42cf0
Evgenia Martynova <evg.martynova@gmail.com>
2023-11-15 14:53:19 +0100

    Clean up the code, add analytical estimate of derivative, throw errors for invalid arguments, add examples, add tests, expand documentation

--- R/AIest.R
+++ R/AIest.R
@@ -1,5 +1,6 @@
-
 #' Attraction Index
+#'
+#'Estimates the Attraction Index function from a point pattern in a window of arbitrary shape.
 #'
 #' @param pp The observed point pattern, from which an estimate of AI(r) will be computed.
 #' An object of class "ppp", or data in any format acceptable to as.ppp().
@@ -15,24 +16,53 @@
 #' @param dim_lims Optional. The integer vector with 2 values: the lower and
 #' upper limits of the possible value of dim, c(lower, upper). lower > upper
 #' is not allowed.
-#' @param dr Optional. The delta r to compute the derivative of the estimated
-#' number of points. If not provided a rule of thumb is supplied, i.e. 1/5 of
-#' the distance between subsequent r values in the K-function estimate is used.
 #' @param rmax Optional. Maximum desired value of the argument.
 #' @param nlarge Optional. Efficiency threshold. If the number of points exceeds
 #' nlarge, then only the border correction will be computed (by default),
 #' using a fast algorithm.
 #'
-#' @return An object of class "fv", see fv.object, which can be plotted directly using plot.fv.
+#' @return An object of class "aifv", inherited from fv.object, which can be
+#' plotted directly using plot.aifv.
 #' @export
 #'
 #' @examples
-#' #TBD
+#'
+#' library(spatstat.random)
+#'
+#' # AI for a random point pattern
+#' rpp <- rpoispp(500)
+#'
+#' ai_rand <- AIest(rpp)
+#' plot(ai_rand, main = "AI for a random point pattern")
+#'
+#' ai <- AIest(rpp, "border")
+#' plot(ai, main = "AI for a random point pattern")
+#'
+#' # AI for a clustered point pattern
+#' clust_pp <- rMatClust(20, 0.05, 25)
+#'
+#' ai_clust <- AIest(clust_pp)
+#' plot(ai_clust, main = "AI for a clustered point pattern")
+#'
+#' # AI for a point pattern with dispersion
+#' hardcore_pp <- rHardcore(300, R=0.05)
+#' ai_disp <- AIest(hardcore_pp)
+#' plot(ai_disp, main = "AI for a point pattern with dispersion")
+#'
+#' # Plot AI for three different point pattern together
+#' plot(ai_rand$r, ai_rand$iso, type="l", ylim=c(-1, 1), col=4)
+#' lines(ai_rand$r, rep(0, nrow(ai_rand)), lty=2)
+#' lines(ai_clust$r, ai_clust$iso, col=2)
+#' lines(ai_disp$r, ai_disp$iso, col=7)
+#' legend("bottomright",
+#'        c("theoretical", "random", "clustered", "dispersed"),
+#'        col=c(1, 4, 2, 7),
+#'        lty=c(2, 1, 1, 1))
+#'
 AIest <- function(pp,
                   correction="Ripley",
                   dim=NULL,
                   dim_lims=NULL,
-                  dr=NULL,
                   rmax=NULL,
                   nlarge=3000) {
 
@@ -40,17 +70,15 @@ AIest <- function(pp,
   # value due to performance reasons
 
   # Check the value
-  if (!is.character(correction)) {
-    rlang::abort("'correction' argument have to have character type")
-  }
-
-  if (length(correction) > 1) {
-    rlang::abort("Due to performance reasons only a single option is allowed
-                 in the 'correction' argument")
+  if (!is.character(correction) || length(correction) > 1) {
+    rlang::abort(class = "ai_error_bad_correction",
+                 message="'correction' argument have to be a character vector
+                 with a single value")
   }
 
   if (correction == "all") {
-    rlang::abort("Won't use all edge correction methods due to performance reasons,
+    rlang::abort(class = "ai_error_inefficient_correction",
+                 message="Won't use all edge correction methods due to performance reasons,
                  choose a single option")
   }
 
@@ -70,37 +98,37 @@ AIest <- function(pp,
   # and then increases monotonously.
   # Only monotonously increasing part is smoothed
   first_non_zero_ind <- which(pn_est_df$pn != 0)[1]
-  pn_est_non_zero <- pn_est_df[first_non_zero_ind:nrow(pn_est_df), ]
+  # Handling the situation when rmax is too large and Kest returns NAs
+  last_ind <- min(nrow(pn_est_df), which(is.na(pn_est_df$pn))[1] - 1, na.rm = TRUE)
+  pn_est_defined <- pn_est_df[first_non_zero_ind:last_ind, ]
 
   if (is.null(dim)) {
     dim <- choose_basis_dim(pp$n, dim_lims=dim_lims)
   }
 
-  if (is.null(dr)) {
-    dr <- k_est$r[2] / 5
-  }
-
-  model <- scam::scam(pn ~ s(r, k=dim, bs='mpi'), data=pn_est_non_zero)
-  deriv_func <- approx_deriv(pn_est_non_zero$r, model, dr)
+  model <- scam::scam(pn ~ s(r, k=dim, bs='mpi'), data=pn_est_defined)
 
   # Smooth a non zero part of the function
-  r_arg <- pn_est_non_zero$r[2:nrow(pn_est_non_zero)]
-  pn_smoothed <- stats::predict(model, newdata=list(r=r_arg))
-  pn_deriv <- deriv_func(r_arg)
-  # Workaround when getting a small negative derivative
+  r_arg <- pn_est_defined$r
+  pn <- stats::predict(model, newdata=list(r=r_arg))
+  pn_deriv <- single_mpi_derivative(model, data=list(r=r_arg))
+
+  # Workaround when getting a small negative derivative (TODO: is it needed now when derivative is analytical?)
   pn_deriv <- dplyr::if_else(pn_deriv >= 0, pn_deriv, 0)
 
-  # Concatenate part of the function where it is 0
-  # and the smoothed part
-  pn <- c(rep(0, first_non_zero_ind), pn_smoothed)
-  pn_deriv <- c(rep(0, first_non_zero_ind), pn_deriv)
-  r <- k_est$r
   ai <- dplyr::if_else(pn > 0 & pn_deriv >= 0,
-                       compute_ai(r, pn, pn_deriv),
+                       compute_ai(r_arg, pn, pn_deriv),
                        -1)
 
+  # Pad the AI value with -1 at distances where there is no neigbours
+  # and with NA when the K function is undefined
+  num_ll_pad <- first_non_zero_ind - 1
+  num_na_pad <- nrow(k_est) - last_ind
+  ai <- c(rep(-1, num_ll_pad), ai, rep(NA_real_, num_na_pad))
+
   ai_df <- data.frame(r=k_est$r,
-                       theo=0)
+                      theo=0)
+
   # Name the column with AI estimate after the used border correction method
   correction_name <- colnames(k_est)[3]
   ai_df[correction_name] <- ai
@@ -110,25 +138,84 @@ AIest <- function(pp,
                                 labl=attr(k_est, "labl"),
                                 desc = attr(k_est, "desc"))
 
+  class(ai_fv) <- c("aifv", class(ai_fv))
+
   ai_fv
 }
 
 
-approx_deriv <- function(x, y_model, dx) {
-  # For now compute derivative with finite diff method, central diff
-  # Then perhaps improve
-  x_breaks <- seq(min(x) + dx / 2, max(x) + dx / 2, dx)
-  y_smoothed <- stats::predict(y_model, newdata=list(r=x_breaks))
+#' Derivative of a scam model with a single MPI term
+#'
+#' @param model A scam model with a single monotone increasing P-spline (MPI)
+#' term
+#' @param data A data frame containing the values of the named covariates
+#' at which the smooth term is to be evaluated.
+#'
+#' @return A vector that contains the derivative of the given scam model at
+#' the valuse provided with the data parameter
+single_mpi_derivative <- function(model, data) {
 
-  y_deriv_num <- diff(y_smoothed) / dx
-  x_data <- x_breaks + dx / 2
-  deriv_formula <- stats::approxfun(x_data[1:(length(x_data) - 1)], y_deriv_num)
+  # check that it is a scam objects
+  if (!inherits(model, "scam")) {
+    rlang::abort(class = "ai_derivative_error_unsupported_model",
+                 message = "Only SCAM objects are supported")
+  }
 
-  deriv_formula
+  # check that there is only one smooth and it is an mpi.smooth
+  if (length(model$smooth) != 1 ||
+      !inherits(model$smooth[[1]], "mpi.smooth") ||
+      length(model$assign) > 1) {
+    rlang::abort(class = "ai_derivative_error_unsupported_model_formula",
+                 message ="Only SCAMs with a slingle mpi smooth terms are supported")
+  }
+
+  # taken from Predict.matrix.mpi.smooth with a modification to extract the derivative
+  smooth <- model$smooth[[1]]
+
+  if (!(smooth$term %in% names(data))) {
+    rlang::abort(class = "ai_derivative_error_missing_argument",
+                 message = "A required argument is not provided in the data")
+  }
+
+  order <- smooth$m + 2
+  q <- smooth$df + 1
+  Sig <- matrix(1, q, q)
+  Sig[upper.tri(Sig)] <- 0
+  ll <- smooth$knots[order]
+  ul <- smooth$knots[length(smooth$knots) - order + 1]
+  x <- data[[smooth$term]]
+  n <- length(x)
+  ind <- x <= ul & x >= ll
+
+  if (sum(ind) != n) {
+    rlang::abort(class = "ai_derivative_error_invalid_argument",
+                 message = "Won't evaluate the derivative outside of the function domain")
+  }
+
+  Xdm <- splines::splineDesign(smooth$knots, x, order, derivs = c(1))
+  gamma_coef <- Sig %*% model$coefficients.t
+
+  Xd <- Xdm %*% gamma_coef
+  c(Xd)
 }
 
 
+#' The number of B-splines to use in the scam model
+#'
+#' @param sample_size Number of points in a point pattern
+#' @param dim_lims Optional. The integer vector with 2 values: the lower and
+#' upper limits of the possible value of dim, c(lower, upper). lower > upper
+#' is not allowed.
+#'
+#' @return An integer value that represents the number of B-splines to use
+#' in a scam model. The rule of thumb dim = sqrt(sample_size) is used and
+#' if dim_lims is provided it is used to clip the value
 choose_basis_dim <- function(sample_size, dim_lims=NULL) {
+
+  if (length(sample_size) > 1 || sample_size < 0 || sample_size %% 1 != 0) {
+    rlang::abort(class = "ai_dim_error_invalid_sample_size",
+                 message = "Sample size should contain a single positive integer value")
+  }
 
   dim <- round(sqrt(sample_size))
 
@@ -136,22 +223,79 @@ choose_basis_dim <- function(sample_size, dim_lims=NULL) {
     return(dim)
   }
 
-  if (length(dim_lims) != 2 || !is.integer(dim_lims)) {
-    rlang::abort("Incorrect dim_lims format, should a the vector of 2 integer values")
+  if (length(dim_lims) != 2 || sum(dim_lims %% 1 != 0) > 0 || sum(dim_lims < 0) > 0) {
+    rlang::abort(class = "ai_dim_error_invalid_dim_lims",
+                 message = "Incorrect dim_lims format, should be a vector of 2 integer values")
   }
 
   if (dim_lims[1] > dim_lims[2]) {
-    rlang::abort("Incorrect dim_lims: the first limit must be lower than the second")
+    rlang::abort(class = "ai_dim_error_unordered_dim_lims",
+                 message = "Incorrect dim_lims, the first limit must be lower than the second")
   }
 
   dim <- min(dim_lims[2], max(dim, dim_lims[1]))
   dim
 }
 
+#' The AI value computation
+#'
+#' @param r A vector of distances
+#' @param pn A vector of estimated expected number of points within a distance r
+#' @param pn_deriv A vector of estimated derivative of the expected number of points
+#' within a distance r
+#' @param ai_lims Optional. The double vector with 2 values: the lower and
+#' upper limits of the AI, c(lower, upper). The lower value corresponds to the AI value
+#' for maximal dispersion, the upper to the AI value for maximal clustering.
+#' lower > upper is not allowed.
+#'
+#' @returns A vector with AI estimate at r
+compute_ai <- function(r, pn, pn_deriv, ai_lims=c(-1, 1)) {
 
-compute_ai <- function(r, pn, pn_deriv, low=-1, high=1) {
-  scale <- high - low
-  shift <- low
+  if (length(ai_lims) != 2 || !is.numeric(ai_lims)) {
+    rlang::abort(class = "ai_compute_error_invalid_ai_lims",
+                 message = "Incorrect ai_lims format, should be a vector of 2 double values")
+  }
+
+  if (ai_lims[1] > ai_lims[2]) {
+    rlang::abort(class = "ai_compute_error_unordered_ai_lims",
+                 message = "Incorrect ai_lims, the first limit must be lower than the second")
+  }
+
+  if (!is.numeric(r) || !is.numeric(pn) || !is.numeric(pn_deriv)) {
+    rlang::abort(class = "ai_compute_error_invalid_arg",
+                 message = "r, pn and pn_deriv have to be double vectors")
+  }
+
+  if (length(r) != length(pn) || length(pn) != length(pn_deriv)) {
+    rlang::abort(class = "ai_compute_error_arg_length_mismatch",
+                 message = "r, pn and pn_deriv vectors should have the same length")
+  }
+
+  scale <- ai_lims[2] - ai_lims[1]
+  shift <- ai_lims[1]
   ai <- exp(-log(2) / 2 * r * pn_deriv / pn) * scale + shift
   ai
+}
+
+#' Plot Function Value for AI
+#'
+#' @param x An object of the class "aifv" that contains the variables to be
+#' plotted.
+#' @param main A title of the plot.
+#' @param ... Extra arguments passed to the spatstat.explore::plot.fv
+#'
+#' @return Invisible: either NULL, or a data frame giving the meaning of the
+#' different line types and colours.
+#' @export
+#' @export plot.aifv
+#'
+#' @examples
+#' library(spatstat.random)
+#'
+#' rpp <- rpoispp(500)
+#' ai_rand <- AIest(rpp)
+#' plot(ai_rand, main = "AI for a random point pattern")
+#'
+plot.aifv <- function(x, main="", ...) {
+  spatstat.explore::plot.fv(x, ylim = c(-1,1), main = main, ...)
 }
---
+++ tests/testthat/helper.R
@@ -0,0 +1,63 @@
+
+# n - number of datapoints
+gen_poisson_data <- function(n) {
+  r <- seq(0.01, 1, length.out=n)
+  pn <- r^2
+  pn_der <- 2*r
+
+  data.frame(r=r, pn=pn, pn_der=pn_der)
+}
+
+# TODO: perhaps change to return the funtions instead
+gen_sigmoid_data <- function(n) {
+  r <- seq(-1, 1, length.out=n)
+  pn <- exp(4 * r) / (1 + exp(4 * r))
+  pn_der <- 4 * exp(4 * r) / (1 + exp(4 * r))^2
+  ai <- exp(-2 * log(2)  * r / (1 + exp(4 * r))) * 2 - 1
+
+  data.frame(r=r, pn=pn, pn_der=pn_der, ai=ai)
+}
+
+
+gen_rpp_in_circle <- function(rad, point_num, centre=c(0, 0)) {
+  circle <- spatstat.geom::disc(radius = rad, centre=centre)
+  circle_a <- spatstat.geom::area(circle)
+  intensity <- point_num / circle_a
+  rpp <- spatstat.random::rpoispp(intensity, win=circle)
+
+  rpp
+}
+
+
+gen_max_clustered_pattern <- function(circle_rad, point_num) {
+  pp_loc <- gen_rpp_in_circle(circle_rad, point_num)
+  wind <- spatstat.geom::owin(c(-0.5, 0.5), c(-0.5, 0.5))
+
+  # Change window to be the target domain
+  pp_mc <- spatstat.geom::ppp(pp_loc$x,
+                              pp_loc$y,
+                              window = wind)
+  pp_mc
+}
+
+
+gen_three_clusters_pattern <- function(circle_rad, circle_point_num, cluster_dist) {
+
+  column_distance <- sqrt(cluster_dist^2 - (cluster_dist / 2)^2)
+  centers <- data.frame(x=c(-cluster_dist / 2, 0, cluster_dist / 2),
+                        y=c(-column_distance/2, column_distance/2, -column_distance/2))
+
+  xs <- NULL
+  ys <- NULL
+  for (i in 1:nrow(centers)) {
+    centre <- c(centers$x[i], centers$y[i])
+    pp <- gen_rpp_in_circle(circle_rad, circle_point_num, centre)
+    xs <- c(xs, pp[["x"]])
+    ys <- c(ys, pp[["y"]])
+  }
+
+  wind <- spatstat.geom::owin(c(-0.5, 0.5), c(-0.5, 0.5))
+  pp_three <- spatstat.geom::ppp(xs, ys, window = wind)
+
+  pp_three
+}
